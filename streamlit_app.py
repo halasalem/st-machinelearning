0292e05ecd53de4b388e5a8378ef42bde77a5013
halasalem <202330054@students.asu.edu.jo>
2025-09-18 15:22:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,18 @@ with st.sidebar:
   bill_depth=st.slider('Bill depth (mm)',13.1,21.5,17.2)
   flipper_length=st.slider('Flipper Length (mm)',172.0,231.0,201.0)
   body_mass=st.slider("Body Mass (g)",2700.0,6300.0,4207.0)
-  
-      
+
+
+data={'island':island,
+      'bill_length_mm':bill_length,
+      'bill_depth_mm':bill_depth,
+      'flipper_length_mm':flipper_length,
+      'body-mass_g':body_mass,
+      'gender':gender
+     }
+
+input df=pd.DataFrame(data, index=[0])
+input penguins=pd.concat([input_df,X],axis=0)
+
+input df
 
