5ed8e0c2b14927565a00e17d8672c95ae55647fb
halasalem <202330054@students.asu.edu.jo>
2025-09-18 14:34:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,17 @@ st.info("This app builds a machine learning model")
 with st.expander("Data Preview"):
   st.write("**Raw data**")
   df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+  df
   
+  st.write("**X**")
+  X=df.drop('species',axis=1)
+  X
+  
+  st.write("**y**")
+  y=df['species']
+  y
 
+with st.expander("Data Visualization"):
+  
+      
 
-
-st.write("**X**")
-X=df.drop('species',axis=1)
-X
-
-st.write("**y**")
-y=df['species']
-y
-          
