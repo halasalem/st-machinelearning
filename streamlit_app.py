def5a496888729f3ffab9190d0e4f9379b6a7880
halasalem <202330054@students.asu.edu.jo>
2025-09-18 14:24:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.info("This app builds a machine learning model")
 
 
 with st.expander("Data Preview"):
-  st.write("**Raw data**)
+  st.write("**Raw data**")
   df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 
 df
